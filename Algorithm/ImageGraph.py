d9df4f3af1024fab8fd0852bbdb33087c03393fd
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-17 09:22:50 +0700

    Global refactoring

--- Algorithm/ImageGraph.py
+++ Algorithm/ImageGraph.py
@@ -1,42 +1,26 @@
 import cv2
 
-import numpy as np
+from PIL import Image
+
+
+def convert_to_binary(image_path, file_name, max_r, max_g, max_b):
+    image = Image.open(image_path)
+    width, height = image.size
+
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            if r < max_r and g < max_g and b < max_b:
+                image.putpixel((x, y), (0, 0, 0))
+            else:
+                image.putpixel((x, y), (255, 255, 255))
+
+    image.save(file_name)
 
 
 def create_graph(image):
-    # Step 1: Load the image into the program and convert it to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    bit_graph = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    contours, hierarchy = cv2.findContours(bit_graph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Step 2: Apply adaptive thresholding to the image
-    max_value = 255
-    block_size = 11
-    binary_image = cv2.adaptiveThreshold(gray, max_value, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                         cv2.THRESH_BINARY, block_size, 1)
-
-    # Step 3: Use a contour detection algorithm to detect the outlines of the graph
-    contours, _ = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Step 4: Remove any contours that are not part of the graph
-    graph_contours = []
-    max_h, max_w = -1, -1
-    height, width, _ = image.shape
-    max_contur = None
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
-        if width * 0.9 > w > max_w and height * 0.9 > h > max_h:
-            max_w = w
-            max_h = h
-            max_contur = contour
-
-    graph_contours.append(max_contur)
-
-    # Step 5: Crop the original image to extract only the graph
-    x, y, w, h = cv2.boundingRect(np.concatenate(graph_contours))
-    graph_image = image[y:y + h, x:x + w]
-
-    bit_graph = cv2.cvtColor(graph_image, cv2.COLOR_BGR2GRAY)
-    bit_graph = cv2.adaptiveThreshold(bit_graph, max_value, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                      cv2.THRESH_BINARY, block_size, 3)
-
-    # Step 6: Save the resulting image with only the function graph
-    return bit_graph
+    cv2.drawContours(bit_graph, contours, -1, (255, 0, 0), 3)
+    cv2.imshow("Image", bit_graph)
--- UI/main.py
+++ UI/main.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
 
-from PIL import Image, ImageTk
-
 from Algorithm import ImageGraph
 
 import cv2
@@ -11,33 +9,19 @@ import cv2
 def open_file():
     file_path = filedialog.askopenfilename()
     if file_path:
-        # Open image
-        image = cv2.imread(file_path)
+        ImageGraph.convert_to_binary(file_path, "binary.jpg", 180, 180, 180)
 
-        # Create graph
-        image = ImageGraph.create_graph(image)
-
-        # Prepare the format
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(image)
-        photo = ImageTk.PhotoImage(image)
-
-        # Update view
-        label.config(image=photo)
-        label.image = photo
+        image = cv2.imread("binary.jpg")
+        ImageGraph.create_graph(image)
 
 
-# Start window
 root = tk.Tk()
 root.title("Выберите фотографию")
 
-# Prepare button
 button = tk.Button(root, text="Выбрать файл", command=open_file)
 button.pack()
 
-# Set init image
 label = tk.Label(root)
 label.pack()
 
-# Open window
 root.mainloop()
